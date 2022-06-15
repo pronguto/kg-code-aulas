acd48ba7259ebdb90db97f628272d00b37419c78
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-06-15 08:23:19 -0300

    aula07

---
+++ aula07/index.js
@@ -0,0 +1,17 @@
+const map2 = [
+  [1, 2, 3, 10, 11],
+  [4, 5, 6, 12, 13, 14],
+  [7, 8, 9],
+];
+
+for (let i = 0; i < map2.length; i++) {
+  // console.log(map2[i].length);
+  for (let j = 0; j < map2[i].length; j++) {
+    // console.log(i);
+    // console.log(j);
+    console.log(map2[i][j]);
+    // console.log("------------------------");
+  }
+  // console.log("**********************");
+}
+// console.table(map2);
